cb49b0964c8e96acc90fc5512cbdd93fa2c28fc6
Thomas Kuntz <tk@thomaskuntz.org>
2024-11-15 20:17:12 +0100

    Add docstring to sensitivity_utils.py

--- src/utils/sensitivity_utils.py
+++ src/utils/sensitivity_utils.py
@@ -3,16 +3,25 @@ import os
 
 api_key_openai = os.getenv("OPENAI_API_KEY")
 
-def verify_sensitivity(article1,article2):
+def verify_sensitivity(article1, article2):
+    """
+    Takes two articles and verifies the sensitivity of the association based on openAI's moderation model omni.
 
-    client = OpenAI(
-        api_key=api_key_openai,
-    )
+    Parameters:
+        article1: Name of the first article
+        article2: Name of the second article
+
+    Returns:
+        response: The raw response of the omni model to the prompt associating articles 1 and 2.
+    """
+
+    client = OpenAI(api_key=api_key_openai)
 
     prompt = str(article1) + ' is associated with ' + str(article2)
 
     response = client.moderations.create(
         model="omni-moderation-latest",
-        input=prompt,
+        input=prompt
     )
+
     return response
