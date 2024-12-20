248367db15f18d8104d25e5f524750fc8418f184
Thomas Kuntz <tk@thomaskuntz.org>
2024-12-20 13:01:43 +0100

    Refactor to include LLM distances with memory

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -20,3 +20,13 @@ def paths_no_backtrack(paths):
         return list(stack)
 
     return paths.apply(__straighten_path)
+
+def get_pairs_with_defined_distance(paths_homing_in_humans):
+    all_distance_pairs = set()
+    all_articles = set()
+    for path in paths_homing_in_humans:
+        goal  = path[-1]
+        for article in path:
+            all_articles.add(article)
+            all_distance_pairs.add((goal, article))
+    return all_distance_pairs, all_articles
